57045c2a852035de061d32d3474f914124ea1deb
k.mitarai <kazmit299@gmail.com>
2020-05-06 04:36:07 +0900

    弾射出コマンド

---
+++ Assets/Script/Enemy/EnemyCommand/Bullet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using Summon;
+
+namespace EnemyAction
+{
+    class Bullet : ICommand
+    {
+        [SerializeField]
+        int ShotNum = 1;
+
+        [SerializeField]
+        float Interval = 0;
+
+        [SerializeField]
+        string BulletName = "Bullet";
+
+        MasterCube Owner;
+        //LinkedList<Summon.Bullet> Summon = new LinkedList<Summon.Bullet>();
+        float Timer = 0.0f;
+        int ShotCount = 0;
+
+        public void Setup(MasterCube owner)
+        {
+            Owner = owner;
+            Timer = 0.0f;
+            ShotCount = 0;
+        }
+
+        public bool Execute()
+        {
+            Timer += Time.deltaTime;
+            if (Timer < Interval) return false;
+
+            while(ShotCount < ShotNum)
+            {
+                var blt = SummonObject.Build<Summon.Bullet>("Bullet", Owner, null, null);
+                blt.transform.position = Owner.transform.position + Owner.transform.forward * 1.5f;
+                blt.AddForce(Owner.transform.forward * 250.0f);
+                //Summon.AddLast(blt);
+
+                ShotCount++;
+                Timer = 0.0f;
+                if (Interval > 0.0f) break;
+            }
+            return (ShotCount >= ShotNum);
+        }
+    }
+}
---
+++ Assets/Script/Summon/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Summon
+{
+    public class Bullet : SummonObject, IUpdatable
+    {
+        [SerializeField]
+        float Life = 1.5f;
+
+        float Timer = 0.0f;
+        Rigidbody RigidBody;
+
+        protected override void Setup()
+        {
+            LifeCycleManager.AddUpdate(this, 0);
+            RigidBody = GetComponent<Rigidbody>();
+            Type = SummonType.Bullet;
+            Timer = 0.0f;
+            base.Setup();
+        }
+
+        public void AddForce(Vector3 force)
+        {
+            RigidBody.AddForce(force);
+        }
+
+        public void UnityUpdate()
+        {
+            Timer += Time.deltaTime;
+            if(Timer >= Life)
+            {
+                LifeCycleManager.RegisterDestroy(this.gameObject);
+            }
+        }
+    }
+}
