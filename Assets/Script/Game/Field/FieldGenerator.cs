304c111647e17413aea6b48c5e0bee28e44eab6b
k.mitarai <kazmit299@gmail.com>
2020-05-11 23:35:07 +0900

    マップ生成のしくみの基礎。 PrimitiveとGameObjectを動的に作る練習

---
+++ Assets/Script/Game/Field/FieldGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using Field;
+
+public class FieldGenerator : MonoBehaviour
+{
+    [Serializable]
+    class KeyPoint
+    {
+        public int KeyId;
+        Vector2 Point;
+    }
+
+    [SerializeField]
+    int FieldSize = 10;
+
+    [SerializeField]
+    int TileSize = 5;
+
+    [SerializeField]
+    List<MapTile> MapTiles = new List<MapTile>();
+
+    [SerializeField]
+    List<KeyPoint> KeyPoints = new List<KeyPoint>();
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        int keyPoint = 0;
+        for (int y = 0; y < FieldSize; ++y)
+        {
+            for (int x = 0; x < FieldSize; ++x)
+            {
+                var mt = new MapTile(TileSize,  -FieldSize/2 + x, -FieldSize / 2 + y, keyPoint, this.transform);
+                MapTiles.Add(mt);
+            }
+        }
+
+        //
+        MapTiles.ForEach(mt => mt.Generate());
+    }
+}
---
+++ Assets/Script/Game/Field/MapTile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Field
+{
+    public class MapTile
+    {
+        public enum TileType
+        {
+            Invalid,
+            Path
+        }
+
+        public class MapChip
+        {
+            public int ChipId;
+            GameObject ChipObject;
+        }
+
+        int Size = 1;
+        int KeyPoint = 0;
+        Transform Parent = null;
+        GameObject Root = new GameObject();
+        List<MapChip> Chips = new List<MapChip>();
+
+        public MapTile(int size, int x, int y, int kp, Transform parent)
+        {
+            Size = size;
+            KeyPoint = kp;
+            Root.name = String.Format("MapTile({0},{1})", x, y);
+            Root.transform.parent = parent;
+            Root.isStatic = true;
+            Root.transform.localPosition = new Vector3(Size * x, 0, Size * y);
+
+            Chips.Capacity = Size * Size;
+            for (int i = 0; i < Size * Size; ++i)
+            {
+                Chips.Add(new MapChip());
+            }
+        }
+
+        //道生成
+        public void GenerateRoute()
+        {
+
+        }
+
+        public void Generate()
+        {
+            for(int y = 0; y < Size; ++y)
+            {
+                for(int x=0; x < Size; ++x)
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Material mat = ResourceCache.GetMaterialCache(ResourceType.FieldMaterial, "Grass");
+
+                    go.isStatic = true;
+                    go.transform.parent = Root.transform;
+                    go.transform.localPosition = new Vector3(-Size/2 + x, 0, -Size/2 + y);
+
+                    //Material差し替え
+                    MeshRenderer mr = go.GetComponent<MeshRenderer>();
+                    mr.material = mat;
+                    mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                    mr.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+                }
+            }
+        }
+    }
+}
