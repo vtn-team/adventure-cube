e9c4e271bc3cc2d16900e092a2b1878ed865ac8e
k.mitarai <kazmit299@gmail.com>
2020-10-08 09:52:35 +0900

    攻撃基底系をコミット

---
+++ Assets/Script/Game/Block/Component/AttackBlock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Block
+{
+    public class AttackBlock : MonoBlock
+    {
+        [SerializeField] float AttackInterval = 1.0f;
+        [SerializeReference, SubclassSelector] IAttackLogic AttackLogic;
+
+        private void Start()
+        {
+        }
+
+    }
+}
