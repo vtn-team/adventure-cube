d05d67ae621f63462dda23da9d023961825f3548
SnowCastle <snowcastle0420@gmail.com>
2020-10-31 20:24:12 +0900

    回復用のキューブの作成 呼び出しはまだ

---
+++ Assets/Script/Game/Block/Implements/Recovery.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Block
+{
+    /// <summary> 回復させてみよう </summary>
+    public class Recovery : MonoBlock , IPassiveBlock
+    {
+        public PassiveType PassiveType => PassiveType.Recovery;
+
+        public int PassiveEvent(int param, int subparam = 0)
+        {
+            return param;
+        }
+
+    }
+
+}
