88edc3e923b1e5c3a18bafa5dd2f045e2d6dda61
murray0809 <tomo19940809@gmail.com>
2020-10-31 20:07:31 +0900

    10/31 課題

---
+++ Assets/Script/Game/Block/Implements/Jump.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Block;
+
+//namespace Block
+//{
+    public class Jump : MonoBlock, IPassiveBlock
+    {
+        public PassiveType PassiveType => PassiveType.Jump;      // タイプを返す実装が必要
+
+        float y;
+
+        bool on = true;
+
+        // パッシブ効果を処理する実装が必要
+        public int PassiveEvent(int param, int subparam)
+        {
+            return figure;
+        }
+
+        void Update()
+        {
+            
+        if (on)
+        {
+            Up();
+        }
+        else
+        {
+            Down();
+        }
+   
+        }
+
+    void Up()
+    {
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+
+        pos.y += 0.5f;
+
+        myTransform.position = pos;
+
+        if (pos.y >= 30)
+        {
+            on = false;
+        }
+    }
+
+    void Down()
+    {
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+
+        pos.y -= 0.5f;
+
+        myTransform.position = pos;
+
+        if (pos.y <= 5f)
+        {
+            on = true;
+        }
+    }
+    }
+//}
+
--- Assets/Script/Game/Block/Interface/IPassiveBlock.cs
+++ Assets/Script/Game/Block/Interface/IPassiveBlock.cs
@@ -18,4 +18,5 @@ public enum PassiveType
 {
     None,
     DamageBuff,
+    Jump,
 }
