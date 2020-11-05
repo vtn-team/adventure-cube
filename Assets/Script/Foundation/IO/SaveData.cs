fca4aa1b9d81d8f22992eaea5b5086a292a04bb1
k.mitarai <kazmit299@gmail.com>
2020-11-06 01:25:23 +0900

    セーブデータとシリアライズ可能辞書

---
+++ Assets/Script/Foundation/IO/SaveData.cs
@@ -0,0 +1,32 @@
+﻿using SerializableCollections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// ローカルセーブ用クラス
+/// </summary>
+[Serializable]
+public class SaveData
+{
+    /// <summary>
+    /// キューブそれぞれが復元に必要な情報を記録する
+    /// </summary>
+    [Serializable]
+    public class CubeData : SerializableDictionary<string, string>
+    {
+            
+    }
+
+    [Serializable]
+    public class MasterCubeSave
+    {
+        public int Type;
+        public List<CubeData> Cubes = new List<CubeData>();
+    }
+
+    public string Version = "0.1";
+    public List<MasterCubeSave> CubeDataList = new List<MasterCubeSave>();
+}
