b39d9af8198b77b5c3a81cf370b4ca4800b409c8
k.mitarai <kazmit299@gmail.com>
2020-11-10 19:47:51 +0900

    オブジェクトプール(作りかけ)

---
+++ Assets/Script/Foundation/Interface/IObjectPool.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public interface IObjectPool
+{
+    bool IsActive { get; }
+    void DisactiveForInstantiate();
+    void Create();
+    void Destroy();
+}
