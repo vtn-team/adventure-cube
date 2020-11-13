f9e060a7c9a7e06c5c3a6116fb1560cc81de3cdd
k.mitarai <kazmit299@gmail.com>
2020-11-13 10:20:12 +0900

    オブジェクトプール調整とソースコードの場所を移動

---
+++ Assets/Script/Foundation/ObjectPool.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Unity上の、特定の型のオブジェクトプール
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ObjectPool<T> where T : UnityEngine.Object, IObjectPool
+{
+    T BaseObj = null;
+    Transform Parent = null;
+    List<T> Pool = new List<T>();
+    int Index = 0;
+        
+    public void SetBaseObj(T obj, Transform parent)
+    {
+        BaseObj = obj;
+        Parent = parent;
+    }
+
+    public void Pooling(T obj)
+    {
+        obj.DisactiveForInstantiate();
+        Pool.Add(obj);
+    }
+
+    public void SetCapacity(int size)
+    {
+        //既にオブジェクトサイズが大きいときは更新しない
+        if (size < Pool.Count) return;
+
+        for(int i = Pool.Count-1; i<size; ++i)
+        {
+            T Obj = default(T);
+            if (Parent)
+            {
+                Obj = GameObject.Instantiate(BaseObj, Parent);
+            }
+            else
+            {
+                Obj = GameObject.Instantiate(BaseObj);
+            }
+            Obj.DisactiveForInstantiate();
+            Pool.Add(Obj);
+        }
+    }
+
+    public T Instantiate()
+    {
+        T ret = null;
+        for (int i = 0; i < Pool.Count; ++i)
+        {
+            int index = (Index + i) % Pool.Count;
+            if(Pool[index].IsActive) continue;
+
+            Pool[index].Create();
+            ret = Pool[index];
+            break;
+        }
+
+        return ret;
+    }
+}
